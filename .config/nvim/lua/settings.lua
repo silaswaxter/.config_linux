d353f52f3987bebabf3ccf23e1b7d41ee9f45d2a
Silas Waxter <silaswaxter@gmail.com>
2022-10-01 11:58:49 -0700

    feat(nvim): enforce 2 tabs and make listchar for tab readable
    Remember nvim's `listchar` for tabs is the characters that will be
    displayed when an actual '\t' character is written in the file.
    It was `>` followed by the number of spaces in a tab, but this
    is really confusing especially when reading assembly. Changed
    to a nerd-font character that is a sequence of vertical dots.

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -2,13 +2,14 @@
 vim.opt.clipboard = "unnamedplus"
 vim.opt.relativenumber = true
 vim.opt.showcmd = true
-vim.opt.listchars = {tab = "> "}
+vim.opt.listchars = {tab = " "}
 vim.opt.list = true
 vim.opt.spell = true
 
 -- File Config (style-guide)
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.encoding = "utf-8"
 vim.opt.colorcolumn = "80"
 vim.opt.textwidth = 80
