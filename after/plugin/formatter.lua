c166e16521eb1f8674ddfd506e63192083619920
Silas Waxter <silaswaxter@gmail.com>
2023-07-10 14:57:58 -0700

    fix(formatter): patch lua-format when :e newfile; config w/ file

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,15 +1,20 @@
 local formatter = require('formatter')
-local util = require 'formatter.util' -- utilities for creating configurations
+local util = require('formatter.util') -- utilities for creating configurations
 
+-- see https://github.com/mhartington/formatter.nvim/issues/253
 local my_luaformat = require('formatter.filetypes.lua').luaformat()
-table.insert(my_luaformat.args, 1, '--indent-width=2')
-table.insert(my_luaformat.args, 1, '--double-quote-to-single-quote')
+my_luaformat.args = {}
 
 formatter.setup({
   logging = true,
   log_level = vim.log.levels.WARN,
 
-  filetype = {lua = {my_luaformat}},
+  filetype = {
+    -- configure `Koihik/LuaFormatter` via files because CLI parameters are 
+    -- prioritized over configuration files; don't want my defaults to override
+    -- a project's local settings.
+    lua = {my_luaformat}
+  },
 
   ['*'] = {require('formatter.filetypes.any').remove_trailing_whitespace}
 })
