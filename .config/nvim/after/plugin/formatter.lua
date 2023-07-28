fa24af23408f1863a7735c8e113b5bb83de101b8
Silas Waxter <silaswaxter@gmail.com>
2023-07-28 13:20:03 -0700

    chore(nvim): delete formatter.lua which was moved to null-ls.lua

--- .config/nvim/after/plugin/formatter.lua
+++
@@ -1,9 +0,0 @@
-local null_ls = require('null-ls')
-
-null_ls.register({
-  null_ls.builtins.formatting.lua_format,
-  null_ls.builtins.formatting.beautysh,
-  null_ls.builtins.formatting.mdformat,
-})
-
--- formatting is done with `vim.lsp.buf.format()`; keybinding set in lsp.lua
